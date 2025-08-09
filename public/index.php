49508fe97859fafed6402f3ecdd9044f5a5e879a
Brian Arindra <brianarindra22@gmail.com>
2025-08-09 19:12:17 +0700

    update

--- public/index.php
+++ public/index.php
@@ -1,6 +1,6 @@
 <?php
 require_once __DIR__ . '/../app/config/database.php';
-require_once __DIR__ . '/../app/controllers/studentController.php';
+require_once __DIR__ . '/../app/controllers/studentcontroller.php';
 
 $database = new Database();
 $db = $database->getConnection();
